dec8b1ebe3ac87d1da8f0263a3839fdd277899a6
Michal Vrbovsky <100531156+VrbovskyM@users.noreply.github.com>
2024-12-04 14:10:57 +0100

    feat: view and display images. Need to do server side rendering and annotations

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Livewire\FullPages\DatasetView;
 use App\Livewire\FullPages\Profile;
 use App\Livewire\FullPages\Welcome;
-use App\Livewire\HelloWorld;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Welcome::class)->name('welcome');
@@ -21,6 +21,6 @@ Route::get('/db-info', function () {
     return config('database.connections.'.config('database.default'));
 });
 
-Route::get('/hello-world', HelloWorld::class);
-
 Route::get('/profile', Profile::class)->name('profile');
+
+Route::get('/dataset/{uniqueName}', DatasetView::class)->name('dataset.view');
