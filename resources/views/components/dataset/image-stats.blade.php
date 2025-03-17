0d38499eeae9e9371ce27bd2e48c9d31dfe68d03
Michal Vrbovsky <100531156+VrbovskyM@users.noreply.github.com>
2025-03-17 22:32:31 +0100

    feat: calcualte median insteaf of avg for iamge sizes

--- resources/views/components/dataset/image-stats.blade.php
+++ resources/views/components/dataset/image-stats.blade.php
@@ -14,8 +14,8 @@
 
         <div class="flex items-center gap-4">
             <div class="flex flex-col items-center">
-                <span class="text-xs text-slate-400">Average</span>
-                <span class="text-sm font-medium text-slate-200">{{ $image_stats['average'] ?? 'N/A' }}</span>
+                <span class="text-xs text-slate-400">Median</span>
+                <span class="text-sm font-medium text-slate-200">{{ $image_stats['median'] ?? 'N/A' }}</span>
             </div>
             <div class="flex flex-col items-center">
                 <span class="text-xs text-slate-400">Min</span>
