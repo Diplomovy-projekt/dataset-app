af9f01c8ed837bee9e282f4741d2a19516b47262
Eugen Antal <antal.87@gmail.com>
2025-05-12 21:16:09 +0200

    fix: metadata seeder language order, missing century

--- database/seeders/MetadataValueSeeder.php
+++ database/seeders/MetadataValueSeeder.php
@@ -20,18 +20,19 @@ class MetadataValueSeeder extends Seeder
         }
 
         $values = [
-            ['metadata_type_id' => $languageType->id, 'value' => 'German'],
-            ['metadata_type_id' => $languageType->id, 'value' => 'Italian'],
-            ['metadata_type_id' => $languageType->id, 'value' => 'Latin'],
-            ['metadata_type_id' => $languageType->id, 'value' => 'Hungarian'],
             ['metadata_type_id' => $languageType->id, 'value' => 'English'],
             ['metadata_type_id' => $languageType->id, 'value' => 'French'],
+            ['metadata_type_id' => $languageType->id, 'value' => 'German'],
+            ['metadata_type_id' => $languageType->id, 'value' => 'Hungarian'],
+            ['metadata_type_id' => $languageType->id, 'value' => 'Italian'],
+            ['metadata_type_id' => $languageType->id, 'value' => 'Latin'],
             ['metadata_type_id' => $languageType->id, 'value' => 'Spanish'],
             ['metadata_type_id' => $centuryType->id, 'value' => '15th Century'],
             ['metadata_type_id' => $centuryType->id, 'value' => '16th Century'],
             ['metadata_type_id' => $centuryType->id, 'value' => '17th Century'],
             ['metadata_type_id' => $centuryType->id, 'value' => '18th Century'],
             ['metadata_type_id' => $centuryType->id, 'value' => '19th Century'],
+            ['metadata_type_id' => $centuryType->id, 'value' => '20th Century'],
             ['metadata_type_id' => $imageGranularityType->id, 'value' => 'Whole (Original)'],
             ['metadata_type_id' => $imageGranularityType->id, 'value' => 'Sub-region (Crop)'],
         ];
